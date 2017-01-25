d241697e9efa64c96401d39b48a6845c4acac1fc
mboy1011 <lyjjieme@gmail.com>
2017-01-25 23:08:52 +0800

    Third Commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
-var add = angular.module("title",[]);
-add.controller('ctrl',
-	function($scope){
-		$scope.todo_list = "Hello";
+angular.module('apptodo',[]) 
+	.controller('TodoCtrl', function(){
+		var control = this;
+  control.todos = [];
+  
+  control.getTotalTodos = function () {
+        return contorl.todo.length;
+  };
+  control.addTodo = function () {
+    control.todos.push({text:control.formTodoText, done:false});
+    control.formTodoText = '';
+  };
+  
+   control.clearCompleted = function () {
+      var oldTodos = control.todos;
+      control.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) control.todos.push(todo);
+      });
+    };
+
 });
+
